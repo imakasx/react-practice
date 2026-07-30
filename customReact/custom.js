3c1eefbad7f9776d2d1019b79f9a471ce6b5a514
imakasx <akashvishwakarma0618@gmail.com>
2026-07-30 23:12:49 +0530

    customReact

--- customReact/custom.js
+++ customReact/custom.js
@@ -1,1 +1,26 @@
-const 
+function customRender (reactElement , container){
+    const element = document.createElement(reactElement.type)
+    element.innerHTML = reactElement.children
+    element.setAttribute('href', reactElement.props.href)
+    element.setAttribute("target", reactElement.props.target)
+    
+    console.log(element)
+    container.appendChild(element)
+
+
+}
+
+
+const reactElement ={
+    type: "a",
+    props :{
+        href : "https://google.com",
+        target: '_blank'
+    },
+    children: "click the link" 
+
+}
+
+const container = document.querySelector('#root');
+
+customRender(reactElement, container);
