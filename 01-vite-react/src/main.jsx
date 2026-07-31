bc2583d403e05c14a46df715fd93a5725cafb0e2
imakasx <akashvishwakarma0618@gmail.com>
2026-07-31 12:10:20 +0530

    react-practice

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,9 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+
+function MyApp(){
+    return(
+        <div>
+            <h1>react or me </h1>
+        </div>
+    )
+}
+// const reactElement ={
+//     type: "a",
+//     props :{
+//         href : "https://google.com",
+//         target: '_blank'
+//     },
+//     children: "click the link" 
+
+// }
+
+// const element = (
+//     <a href="https://google.com" target='_blink'>vist the link</a>
+// )
+
+const element = React.createElement(
+    'a',
+    {href:'https://google.com', target:'_blink'},
+    'vist link'
+)
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    element
 
 )
--- customReact/custom.js
+++ customReact/custom.js
@@ -1,15 +1,24 @@
 function customRender (reactElement , container){
-    const element = document.createElement(reactElement.type)
-    element.innerHTML = reactElement.children
-    element.setAttribute('href', reactElement.props.href)
-    element.setAttribute("target", reactElement.props.target)
+    // const element = document.createElement(reactElement.type)
+    // element.innerHTML = reactElement.children
+    // element.setAttribute('href', reactElement.props.href)
+    // element.setAttribute("target", reactElement.props.target)
     
+    // console.log(element)
+    // container.appendChild(element)
+
+
+    const element = document.createElement(reactElement.type)
     console.log(element)
+    element.innerHTML  = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue // line is not importent just testing for work 
+        element.setAttribute(prop, reactElement.props[prop])
+        
+    }
     container.appendChild(element)
 
-
 }
-
 
 const reactElement ={
     type: "a",
