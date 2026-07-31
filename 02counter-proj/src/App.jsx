cfc459a38960b2a808a801407eb20a3b3cd06034
imakasx <akashvishwakarma0618@gmail.com>
2026-07-31 22:47:10 +0530

    react

---
+++ 02counter-proj/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is react</h1>
+      <h2>Counter value: 5</h2>
+
+      <button>Add value</button>
+      <br />
+      <button>Remove value</button>
+    </>
+  )
+}
+
+export default App
