e4b4fda8002ae27d188603c3935387496fbb1677
imakasx <akashvishwakarma0618@gmail.com>
2026-08-01 23:02:55 +0530

    tailwind

--- 03-tailwind/src/App.jsx
+++ 03-tailwind/src/App.jsx
@@ -7,7 +7,26 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Tailwind CSS</h1>
+      <h1 className='bg-green-800 text-black p-4 rounded-3xl'>Tailwind CSS</h1>
+    <div className="w-60 flex flex-col rounded-xl bg-black min-h-[19rem] text-white">
+      <div>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
+          alt="test"
+          className="object-cover object-center rounded-t-xl"
+        />
+      </div>
+      <div className="flex flex-col py-3 px-3 pb-10">
+        <div className="flex justify-between ">
+          <h1 className="font-bold ">Bored ape nft accidental</h1>
+          <h1>Price</h1>
+        </div>
+        <div className="flex  justify-between">
+          <p>#345</p>
+          <p>0.01</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
