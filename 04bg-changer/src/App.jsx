6ed13b259869260b3050bc5805e92226b292e7fe
imakasx <akashvishwakarma0618@gmail.com>
2026-08-03 11:23:58 +0530

    update

--- 04bg-changer/src/App.jsx
+++ 04bg-changer/src/App.jsx
@@ -10,7 +10,12 @@ function App() {
     <>
     <div className='w-full h-screen duration-150' 
     style={{background : color}}>
-      
+      <div className='fixed flex flex-wrap justify-center bottom-20 inset-x-0 px-2'>
+        <div className='fixed flex flex-wrap justify-center bg-white rounded-2xl px-2 py-1' >
+          
+        </div>
+      </div>
+
     </div>
     </>
   )
