37717794d3f951653e07b0af25528a1a59732cd4
imakasx <akashvishwakarma0618@gmail.com>
2026-08-05 23:31:00 +0530

    update

--- 05-password-gen/src/App.jsx
+++ 05-password-gen/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
   const [length, setlength] = useState(8)
+  const [char, setchar] = useState(false)
+  const [num, setnum] = useState(false)
+  const [pass, setpass] = useState("")
+
+  const PasswordGen = useCallback(()=>{
+    let Password = "";
+    let str = "ABCDEFGHIZKLMNOPQSTRUVWXYZabxdefghizklmnopqrstuvwxyz";
+    if(num)str += "0123456789";
+    if(num)char += "[]+_!@#$%^&*{}~";
+    
+    for (let i = 1; i <= array.length; i++) {
+      let word = Math.floor(Math.random() * str.length + 1)
+      Password += str.charAt(word)
+    }
+    setpass(Password) 
+    },[length, setchar, setpass, setnum])
 
   return (
   <>
   <h1 className='text-4xl text-center text-white '>Password Generator</h1>
+  
   </>
   )
 }
