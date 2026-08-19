58b39707eeff81e7161b3fe90d8bd96c281b6a34
imakasx <akashvishwakarma0618@gmail.com>
2026-08-19 21:12:27 +0530

    react-router

--- 07-react-router/src/components/Footer/Footer.jsx
+++ 07-react-router/src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/imakasx"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -71,7 +71,7 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        <a href="" className="hover:underline">
                             akashvishwakarma
                         </a>
                         . All Rights Reserved.
