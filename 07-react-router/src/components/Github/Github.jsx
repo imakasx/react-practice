090a7b5631b8942185af397b9c77b680c7837efe
imakasx <akashvishwakarma0618@gmail.com>
2026-08-19 20:50:01 +0530

    react-router

--- 07-react-router/src/components/Github/Github.jsx
+++ 07-react-router/src/components/Github/Github.jsx
@@ -1,16 +1,20 @@
-import { useEffect ,  useState } from "react"
+// import { useEffect ,  useState } from "react"
+
+import { useLoaderData } from "react-router-dom"
 
 function Github() {
-    const [Data , setData] = useState([])
-    useEffect(() =>{
-         fetch('https://api.github.com/users/imakasx')
-        .then(response => response.json())
-        .then(data => 
-        // console.log(data)
-        setData(data)
-    )
-    },[])
+    // const [Data , setData] = useState([])
+    // useEffect(() =>{
+    //      fetch('https://api.github.com/users/imakasx')
+    //     .then(response => response.json())
+    //     .then(data => 
+    //     // console.log(data)
+    //     setData(data)
+    // )
+    // },[])
    
+
+    const Data = useLoaderData()
 
     return(
         <div className="text-center">
@@ -20,3 +24,8 @@ function Github() {
     )
 }
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch ('https://api.github.com/users/imakasx')
+    return response.json()
+}
