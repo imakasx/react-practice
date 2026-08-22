c6562861ee9fc77027a9ab94f2eb4179e4b2fe7e
imakasx <akashvishwakarma0618@gmail.com>
2026-08-22 19:56:22 +0530

    theme-switcher

--- 09-theme-switcher/src/App.jsx
+++ 09-theme-switcher/src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { ThemeProvide } from "./context/Theme";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
-                    </div>
+    <ThemeProvide>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme button */}
 
-                    <div className="w-full max-w-sm mx-auto">
-                       
-                    </div>
-                </div>
-            </div>
-    </>
-  )
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+        </div>
+      </div>
+    </ThemeProvide>
+  );
 }
 
-export default App
+export default App;
---
+++ 09-theme-switcher/src/context/Theme.js
@@ -0,0 +1,13 @@
+import { useContext, createContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode : "Light",
+    darkTheme :()=>{},
+    lightTheme :()=>{}
+})
+
+export const ThemeProvide = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
