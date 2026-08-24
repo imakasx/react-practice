cfefcccbc732be899729e9c18bd3cffbfea45737
imakasx <akashvishwakarma0618@gmail.com>
2026-08-24 23:02:57 +0530

    update

--- 10-todolist-Local/src/App.jsx
+++ 10-todolist-Local/src/App.jsx
@@ -4,13 +4,22 @@ import './App.css'
 
 function App() {
   const [todos , setTodos]= useState([])
+
   const addTodo = (todo)=>{
     setTodos((prev)=>[{id: Date.now() , ...todo},...prev])
   }
+
   const updateTodo = (id , todo)=>{
     setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? todo : prevTodo)))
   }
   
+  const deleteTodo = (id) =>{
+
+  }
+
+  const toggleComplete = (id)=>{
+
+  }
   return (
     <TodoProvider value={{todos, addTodo , updateTodo ,deleteTodo, toggleComplete}}>
     <div className="bg-[#172842] min-h-screen py-8">
