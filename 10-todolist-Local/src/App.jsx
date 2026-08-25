103ff24e1d7c51be23f73d8a8c4485f8e90fa0fe
imakasx <akashvishwakarma0618@gmail.com>
2026-08-25 19:24:53 +0530

    update

--- 10-todolist-Local/src/App.jsx
+++ 10-todolist-Local/src/App.jsx
@@ -14,11 +14,11 @@ function App() {
   }
   
   const deleteTodo = (id) =>{
-
+    setTodos((prev)=> prev. filter((todo)=> todo.id !== id))
   }
 
   const toggleComplete = (id)=>{
-
+    setTodos((prev)=> prev.map((prevTodo)=>prevTodo === id?{...prevTodo, completed:!prevTodo.completed}:prevTodo))
   }
   return (
     <TodoProvider value={{todos, addTodo , updateTodo ,deleteTodo, toggleComplete}}>
