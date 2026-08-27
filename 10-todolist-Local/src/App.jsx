9620755bf70aea66a8049399e7f1a7c13b13f976
imakasx <akashvishwakarma0618@gmail.com>
2026-08-27 22:46:43 +0530

    update

--- 10-todolist-Local/src/App.jsx
+++ 10-todolist-Local/src/App.jsx
@@ -1,6 +1,8 @@
 import {TodoProvider} from "./contexts"
 import { useEffect, useState } from 'react'
 import './App.css'
+import { TodoFrom } from "./component/Index"
+import TodoItem from "./component/Todoitem"
 
 function App() {
   const [todos , setTodos]= useState([])
@@ -42,9 +44,15 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoFrom/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id} className="w-full">
+                            <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
--- 10-todolist-Local/src/component/Todoitem.jsx
+++ 10-todolist-Local/src/component/Todoitem.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useState } from "react";
+import { useTodo } from "../contexts";
 
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg]= useState(todo.todo)
+    const {deleteTodo,  toggleComplete , updateTodo , addTodo} = useTodo
+
+    const editTodo = () => {
+        updateTodo(todo.id , {...todo , todo: todoMsg})
+        setIsTodoEditable(false)
+    }
+    const toggleCompleted = ()=>{
+        toggleComplete(todo.id)
+    }
 
     return (
         <div
@@ -13,7 +24,7 @@ function TodoItem({ todo }) {
                 type="checkbox"
                 className="cursor-pointer"
                 checked={todo.completed}
-                onChange={toggleCompleted}
+                onChange={toggleComplete}
             />
             <input
                 type="text"
