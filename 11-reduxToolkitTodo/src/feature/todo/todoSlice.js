2e7f17260d28addee9e8d9b316ffdaa2b0cb230c
imakasx <akashvishwakarma0618@gmail.com>
2026-08-30 23:20:29 +0530

    update-code

--- 11-reduxToolkitTodo/src/feature/todo/todoSlice.js
+++ 11-reduxToolkitTodo/src/feature/todo/todoSlice.js
@@ -1,17 +1,32 @@
 import {createSlice , nenoid} from '@reduxjs/toolkit'
+import { act } from 'react'
 
 const initialState = {
     todos:[{id:1, text:"hey im akash "}]
 }
 
-function sayHello (){
-    console.log("hello  ")
-}
+// function sayHello (){
+//     console.log("hello  ")
+// }
 
 export const todoSlice = createSlice({
     name : 'todo', 
     initialState, 
     reducers:{
-        addTodo:
+        addTodo: (state , action )=>{
+            const todo = {
+                id : nenoid(),
+                text : action.payload   
+            }, 
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action )=>{
+            state.todos = state. todos.filter((todo)=>todo.
+        id !== action.payload   )
+        },
     }
 })
+
+export const {addTodo, removeTodo} = todoSlice.actions 
+
+export default todoSlice.reducer
