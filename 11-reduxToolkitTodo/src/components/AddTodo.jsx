eca8db02633be35398e22fd5d902a3dc7c106897
imakasx <akashvishwakarma0618@gmail.com>
2026-08-31 17:55:00 +0530

    update-Todos

--- 11-reduxToolkitTodo/src/components/AddTodo.jsx
+++ 11-reduxToolkitTodo/src/components/AddTodo.jsx
@@ -5,10 +5,11 @@ import {addTodo} from "../feature/todo/todoSlice"
 function AddTodo(){
     const[input , setInput] = useState("")
     const dispatch = useDispatch()
-    
+
     const addTodoHandler = (e)=>{
         e.preventDefault()
         dispatch(addTodo(input))
+        setInput('')
     }
     return(
         <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
--- 11-reduxToolkitTodo/src/components/Todos.jsx
+++ 11-reduxToolkitTodo/src/components/Todos.jsx
@@ -1,4 +1,8 @@
+import {useSelector, useDispatch} from 'react-redux'
+import { removeTodo } from '../feature/todo/todoSlice'
 export function Todos(){
+    const todos =useSelector (state=>state.Todos)
+    const dispatch = useDispatch()
     return(
         <div>T</div>
     )
