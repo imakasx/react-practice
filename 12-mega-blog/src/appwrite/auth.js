213bbd52d1877b02bbafdcc7ae2abcfcfe29e441
imakasx <akashvishwakarma0618@gmail.com>
2026-09-07 21:58:42 +0530

    update-code

--- 12-mega-blog/src/appwrite/auth.js
+++ 12-mega-blog/src/appwrite/auth.js
@@ -1,15 +1,42 @@
 import conf from "../config/conf";
 
-import {client , account } from "appwrite";
+import {Client , Account } from "appwrite";
 
-const appwriteClient = new client.Client()
-    .setEndpoint(conf.appwriteUrl)
-    .setProject(conf.appwriteProjectId);
-    
-const account = new account(appwriteClient);
+export class AuthService {
+    client = new Client();
+    account;
 
-const user = await account.create(
-    ID: "unique()",
-    "email@example.com",
-    "password",
-)
+    constructor() {
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount(email, password, name ){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount) {
+                return this.login(email, password);
+            } else {
+                return userAccount;
+            }
+        }catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email, password) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+
+const authService = new AuthService();
+
+export default AuthService;
