96ad55e16367676e1cfc8b6e78d4b49381b0b4d9
imakasx <akashvishwakarma0618@gmail.com>
2026-09-09 20:20:27 +0530

    update

--- 12-mega-blog/src/appwrite/auth.js
+++ 12-mega-blog/src/appwrite/auth.js
@@ -34,6 +34,15 @@ export class AuthService {
             throw error;
         }
     }
+
+    async getCurrentUser() {
+        try{
+            return await this.account.get();
+        } catch (error){
+            console.log ("Appwrite getCurrentUser error: ", error);
+        }
+        return null;
+}
 }
 
 
