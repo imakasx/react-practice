2b338393d47b82796f7421289dc1a57fa1cb18ac
imakasx <akashvishwakarma0618@gmail.com>
2026-09-10 19:40:36 +0530

    update

---
+++ 12-mega-blog/src/appwrite/config.js
@@ -0,0 +1,8 @@
+import Conf from '../config/conf.js';
+import { Client, ID, Databases, Storage, Query } from 'appwrite';
+
+export class AppwriteService {
+
+}
+const Service = new Service();
+export default Service
