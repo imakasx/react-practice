86611803d49f9050390cb1a7be8d6648a086ee48
imakasx <akashvishwakarma0618@gmail.com>
2026-09-10 20:01:58 +0530

    update-config

--- 12-mega-blog/src/appwrite/config.js
+++ 12-mega-blog/src/appwrite/config.js
@@ -1,4 +1,4 @@
-import Conf from '../config/conf.js';
+import conf from '../config/conf.js';
 import { Client, ID, Databases, Storage, Query } from 'appwrite';
 
 export class AppwriteService {
@@ -9,6 +9,60 @@ export class AppwriteService {
         this.clinet
         .setEndpoint(Conf.appwriteUrl)
         .setProject(Conf.appwriteProjectId);
+        this.databases = new Databases(this.clinet);
+        this.bucket = new Storage(this.clinet);
+    }
+    async creatPost(title, content, slug, featuredImage, userId, status){
+        try{
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        }catch(error){
+            console.log("Appwrite service :: createPost :: error", error)
+        }
+    }
+    async updatePost(slug,{
+        title,
+        content,
+        featuredImage,
+        status,
+    }){
+        try{
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug, 
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            )
+        }catch(error){
+            console.log("Appwrite service :: deletePost :: error", error)
+        }
+    }
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        }catch(error){
+            console.log("Appwrite service :: deletePost :: error", error)
+        }
     }
 }
 const Service = new Service();
