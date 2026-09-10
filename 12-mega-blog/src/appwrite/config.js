bfaf079a30e883b05ab63266cbbacd6dcc64052c
imakasx <akashvishwakarma0618@gmail.com>
2026-09-10 19:44:02 +0530

    fix-bug

--- 12-mega-blog/src/appwrite/config.js
+++ 12-mega-blog/src/appwrite/config.js
@@ -2,7 +2,14 @@ import Conf from '../config/conf.js';
 import { Client, ID, Databases, Storage, Query } from 'appwrite';
 
 export class AppwriteService {
-
+    clinet = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.clinet
+        .setEndpoint(Conf.appwriteUrl)
+        .setProject(Conf.appwriteProjectId);
+    }
 }
 const Service = new Service();
 export default Service
