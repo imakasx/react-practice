9efd42b22157c0edf5e52428ba1b40d232d49e4c
imakasx <akashvishwakarma0618@gmail.com>
2026-09-12 22:42:42 +0530

    deletefile-update

--- 12-mega-blog/src/appwrite/config.js
+++ 12-mega-blog/src/appwrite/config.js
@@ -105,6 +105,19 @@ export class AppwriteService {
             return false
         }
     }
+
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        }catch(error){
+            console.log("Appwrite service :: deleteFile :: error", error)
+            return false
+        }
+    }
 }
 const Service = new Service();
 export default Service
