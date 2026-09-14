20d5621519b697c9d076433b2a8cc3834793cc4d
imakasx <akashvishwakarma0618@gmail.com>
2026-09-14 20:46:00 +0530

    update and add files

--- 12-mega-blog/src/App.jsx
+++ 12-mega-blog/src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading , setloading] = useState(true)
+  const dispatch = useDispatch() 
 
-  return (
-    <>
-      <h1>A blog app in appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setloading(false))
+  },[])
+  return !loading ? (
+    <div className='min-h-screen'>hello</div>
+  ): null
 }
 
 export default App
