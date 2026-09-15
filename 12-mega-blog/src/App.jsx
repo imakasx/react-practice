c8cd21ac0c4f8187b0a212b3787e3e49f75b0049
imakasx <akashvishwakarma0618@gmail.com>
2026-09-15 22:00:21 +0530

    fix-code

--- 12-mega-blog/src/App.jsx
+++ 12-mega-blog/src/App.jsx
@@ -21,23 +21,18 @@ function App() {
     })
     .finally(()=> setloading(false))
   },[])
-  // return ! loading ? (
-  //   <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-  //       <div className='w-full block'>
-  //         <Header />
-  //         <main>
-  //           {/*<Outlet/> */}
-  //         </main>
-  //         <Footer />
-  //       </div>
-  //     </div>
-  // ): null
+  return ! loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
 
-  return (<div>
-    <Header/>
-    <Footer/>
-  </div>
-  )
+          <Header />
+          <main>
+            {/*<Outlet/> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
+  ): null
 }
 
 export default App
