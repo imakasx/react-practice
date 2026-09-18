1f8ca2d34f9981d9bbf48034fbf7128910e42fe4
imakasx <akashvishwakarma0618@gmail.com>
2026-09-18 19:22:32 +0530

    importfile

--- 12-mega-blog/src/components/Header/LogoutBtn.jsx
+++ 12-mega-blog/src/components/Header/LogoutBtn.jsx
@@ -1,3 +1,6 @@
+import {useDispatch} from "react-redux"
+import authService from "../../appwrite/auth"
+import {logout} from "../../store/authSlice"
 function LogoutBtn(){
     return(
         <div>LogoutBtn</div>
