85ae74c515f859e64729bbb3b5b0377e54efcd74
imakasx <akashvishwakarma0618@gmail.com>
2026-09-19 21:57:30 +0530

    update-code-logoutbtn

--- 12-mega-blog/src/components/Header/LogoutBtn.jsx
+++ 12-mega-blog/src/components/Header/LogoutBtn.jsx
@@ -4,10 +4,14 @@ import {logout} from "../../store/authSlice"
 function LogoutBtn(){
     const dispatch = useDispatch()
     const logoutHandler = ()=>{
-        authService.logout()
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
     }
     return(
-        <div>LogoutBtn</div>
+        <button className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+            Logout
+        </button>
     )
 }
 
