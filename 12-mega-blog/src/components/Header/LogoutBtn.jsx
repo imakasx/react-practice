22be5276497c0ab8c9d14d01314e95019f3f8805
imakasx <akashvishwakarma0618@gmail.com>
2026-09-19 21:54:08 +0530

    update-logoutbtn

--- 12-mega-blog/src/components/Header/LogoutBtn.jsx
+++ 12-mega-blog/src/components/Header/LogoutBtn.jsx
@@ -2,6 +2,10 @@ import {useDispatch} from "react-redux"
 import authService from "../../appwrite/auth"
 import {logout} from "../../store/authSlice"
 function LogoutBtn(){
+    const dispatch = useDispatch()
+    const logoutHandler = ()=>{
+        authService.logout()
+    }
     return(
         <div>LogoutBtn</div>
     )
