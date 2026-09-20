09bde52fafc8afcfb69f6058aa87ee5d1965558e
imakasx <akashvishwakarma0618@gmail.com>
2026-09-20 21:07:50 +0530

    update-Header

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -5,6 +5,18 @@ import { useNavigate } from 'react-router-dom'
 
 
 function Header() {
+    const authStatus = useSelector((state )=> state.auth.status)
+    const navigate = useNavigate()
+    const navItem = [
+        {
+            name : 'Home',
+            slug : '/',
+            active :true
+        },{},{},{}
+
+    ]
+    
+
 
     return(
         <div>Header</div>
