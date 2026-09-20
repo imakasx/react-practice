2ca24b637dbe12e035a902067f3b3b954630b825
imakasx <akashvishwakarma0618@gmail.com>
2026-09-20 21:10:27 +0530

    update

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -1,25 +1,39 @@
-import {LogoutBtn , Logo , Container} from '../index'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
+import { LogoutBtn, Logo, Container } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-    const authStatus = useSelector((state )=> state.auth.status)
-    const navigate = useNavigate()
+    const authStatus = useSelector((state) => state.auth.status);
+    const navigate = useNavigate();
     const navItem = [
-        {
-            name : 'Home',
-            slug : '/',
-            active :true
-        },{},{},{}
+    {
+    name: "Home",
+    slug: "/",
+    active: true,
+    },
+    {
+    name: "Login",
+    slug: "/login",
+    active: !authStatus,
+    },
+    {
+    name: "Signup",
+    slug: "/signup",
+    active: !authStatus,
+    },
+    {
+    name: "All Posts",
+    slug: "/all-posts",
+    active: authStatus,
+    },
+    {
+    name: "Add Post",
+    slug: "/add-post",
+    active: authStatus,
+    },
+];
 
-    ]
-    
-
-
-    return(
-        <div>Header</div>
-    )
+return <div>Header</div>;
 }
-export default Header
+export default Header;
