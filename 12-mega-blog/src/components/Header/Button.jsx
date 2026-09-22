886a3caa0aa0b49f88b7ac7d052d5047d48b0152
imakasx <akashvishwakarma0618@gmail.com>
2026-09-22 22:12:35 +0530

    update

--- 12-mega-blog/src/components/Header/Button.jsx
+++ 12-mega-blog/src/components/Header/Button.jsx
@@ -1,7 +1,18 @@
 
-function Button(){
+function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+
+}){
     return(
-        <div>Button</div>
+        <button className={'px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}'} 
+        {...props}>
+            {children}
+        </button>
     )
 }
 export default Button
