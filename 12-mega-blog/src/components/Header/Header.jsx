22c643682069c0f4f7ff240549e02e0c04b9420f
imakasx <akashvishwakarma0618@gmail.com>
2026-09-22 21:58:18 +0530

    add classname

--- 12-mega-blog/src/components/Header/Header.jsx
+++ 12-mega-blog/src/components/Header/Header.jsx
@@ -48,7 +48,8 @@ return(
                     { navItem.map((item) => 
                     item.active ? (
                         <li key={item.name}>
-                            <button onClick={()=> navigate(item.slug)}>
+                            <button onClick={()=> navigate(item.slug)} 
+                            className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
                                 {item.name}
                             </button>
                         </li>
