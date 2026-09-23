d96951d7861870b4155b1d185fb1ddd8c070347f
imakasx <akashvishwakarma0618@gmail.com>
2026-09-23 23:19:48 +0530

    update-code

--- 12-mega-blog/src/components/Header/Input.jsx
+++ 12-mega-blog/src/components/Header/Input.jsx
@@ -1,9 +1,20 @@
 import React , {useId} from "react"
 
-function Input({}){
-    const id = useId()
-    return(
-        <div>Input</div>
+const Input = React.forwardRef(function input({
+    label, 
+    type= "text",
+    className = "",
+    ...props
+}, ref){
+    const id = useId( )
+    return (
+    <div className="w-full">
+        {label && <label
+        className="inline-block mb-1 pl-1"
+        htmlFor={id}>
+            {label}
+            </label>}
+    </div>
     )
-}
+})
 export default Input
