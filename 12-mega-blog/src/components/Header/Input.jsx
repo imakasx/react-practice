13e451f7b46f74a444755fb3de6d278cb0cba77c
imakasx <akashvishwakarma0618@gmail.com>
2026-09-24 23:31:47 +0530

    input-classname-update

--- 12-mega-blog/src/components/Header/Input.jsx
+++ 12-mega-blog/src/components/Header/Input.jsx
@@ -15,7 +15,8 @@ const Input = React.forwardRef(function input({
             {label}
             </label>}
             <input type={text}
-            className={`${className}`}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+                ${className}`}
             ref= {ref}
             />
     </div>
