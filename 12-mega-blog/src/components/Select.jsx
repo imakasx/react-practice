c8dfb8b6918597141023bd55c3ba9884377f4709
imakasx <akashvishwakarma0618@gmail.com>
2026-09-25 22:10:37 +0530

    add-tailwind

--- 12-mega-blog/src/components/Select.jsx
+++ 12-mega-blog/src/components/Select.jsx
@@ -9,7 +9,8 @@ function Select({
     return(
         <div>
             {label && <label htmlFor={id} className=""> </label>}
-            <select ref={ref} id={id} className={``}></select>
+            <select ref={ref} id={id} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+                ${className}`}></select>
         </div>
     )
 }
